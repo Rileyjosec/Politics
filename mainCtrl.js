66edeeea3ee936acba9c471dabb7921603368d79
Riley Carrasquillo <rileyjosec@gmail.com>
2015-06-13 12:49:02 -0600

    Did a bunch of css and Styling

--- mainCtrl.js
+++ mainCtrl.js
@@ -1,8 +1,25 @@
 var app = angular.module('polApp');
 
 app.controller('mainCtrl', function ($scope, polService) {
+	$scope.polly = 'Place Last name of Senator here';
+	
+	$scope.masterList = {};
 	$scope.getVotes = function () {
-		polService.getVotes('Orrin');
-	}
+		
+		polService.searchLegislators($scope.polly)
+		.success(function(data){
+			$scope.legislators = data.results;
+			
+			//search the array to find the first exact match on a name
+//			polService.getVotes('someSpecificBioGuide').then(function(arrayOfHisVotes){
+//			
+//			})
+		})
+		.error(function(err){
+			console.log(err)
+		})
+	};
+	
+	
 	console.log('Hey there punk');
 })
--- polService.js
+++ polService.js
@@ -2,36 +2,27 @@
 
 var app = angular.module('polApp');
 
+
 app.service('polService', function ($http) {
 	var apiKey = '&apikey=1eef97e1c97c4e4fb29ba8541eb7cd49';
-	this.getVotes = function (name) {
-			$http({
+	this.searchLegislators = function (name) {
+			return $http({
 				method: 'GET',
 				url: 'https://congress.api.sunlightfoundation.com/legislators?query=' + name  + apiKey
 			})
 			.success(function (res) {
-				return res.results[1].bioguide_id;
+				return res.results;
 			})
-			.then(function (id){
-					return $http ({
-					method: 'GET',
-					url: 'https://congress.api.sunlightfoundation.com/votes?voter_ids.' + id + '__exists=true' + apiKey
-				})
-			})
-			.then (function() {
-				
-			})
-
-				
-//				.then(function (id) {
-//				$http({
+			
+			
+//			.then(function (id){
+//					return $http ({
 //					method: 'GET',
 //					url: 'https://congress.api.sunlightfoundation.com/votes?voter_ids.' + id + '__exists=true' + apiKey
 //				})
-//				.success(function (res) {
-//					votePromise.resolve;
-//				})
-//				return votePromise;
-//			});
+//			})
+//			.then (function(data) {
+//				console.log(data.data);
+//			})
 		};
 	});
