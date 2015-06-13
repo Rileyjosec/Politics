42a638c4084cf42aedffd3098d8b7fd95d12895d
Riley Carrasquillo <rileyjosec@gmail.com>
2015-06-13 13:01:41 -0600

    Added some Bootstrap and Started routing

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 var app = angular.module("polApp", ['ngRoute']);
 
 
-//app.config(function($routeProvider){
-//        $routeProvider.
-//            when('/login', {
-//                templateUrl: 'pages/login/login.html'
-//            }).
-//            when('/main', {
-//                templateUrl: 'pages/main/main.html'
-//            }).
-//            when('/thread/:threadId', {
-//                templateUrl: 'pages/thread/thread.html'
-//            }).
-//            otherwise({
-//                redirectTo: '/login'
-//            });
-//    });
+app.config(function($routeProvider){
+        $routeProvider.
+            when('/login', {
+                templateUrl: 'searchLegi/mainPage.html'
+            }).
+            when('/billSumm', {
+                templateUrl: 'billSumm/billSumm.html'
+            }).
+            otherwise({
+                redirectTo: '/login'
+            });
+    });
     
     
